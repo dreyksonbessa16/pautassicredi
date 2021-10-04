ed87b03f3be4db855d3a0724d742929a98308991
Dreykson Bessa <dreyksonbessa16@hotmail.com>
2021-10-04 20:59:58 -0300

    create routes

---
+++ routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const rotaPautas = require('./routes/pautas');
+
+app.use('/pautas', rotaPautas);
+
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
+
+module.exports = app;
